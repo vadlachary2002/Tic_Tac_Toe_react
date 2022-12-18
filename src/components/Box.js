011b6981622b895b2e0ee6d25e42694e312f2181
VADLACHARY <76141959+vadlachary2002@users.noreply.github.com>
2022-12-18 19:12:11 +0530

    added logic function
    added logic function to increase the pointer of the players

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,9 +1,9 @@
 import React from "react"
 import "./Box.css"
-export const Box =  ({value}) => {
-  const boxStyle= value==="x"?"box x":"box o";
+export const Box =  ({value,onClick}) => {
+  const boxStyle= value=="X"?"box x":"box o";       // for styling the x box and o box
   return(
-    <button className="box">{value}</button>
+    <button className={boxStyle} onClick={onClick}>{value}</button>
 
   )
 
